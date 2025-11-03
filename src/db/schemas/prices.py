26c9e419eeb96c4d06e54e36ce8a80153785f07d
Adam Wittmann <adamjwittmann@gmail.com>
2025-11-03 18:55:02 -0500

    create prices table

---
+++ src/db/schemas/prices.py
@@ -0,0 +1,22 @@
+from sqlalchemy import MetaData, Table, Column, Integer, String, Date, Float
+
+metadata = MetaData()
+
+prices = Table(
+    "prices",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("symbol", String),
+    Column("date", Date),
+    Column("open", Float),
+    Column("high", Float),
+    Column("low", Float),
+    Column("close", Float),
+    Column("Volume", Float),
+    Column("sma_20", Float),
+    Column("sma_50", Float),
+    Column("sma_200", Float),
+    Column("daily_return", Float),
+    Column("volatility", Float),
+    Column("drawdown", Float)
+)
